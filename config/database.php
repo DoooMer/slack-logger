665461b853c4e08506d48deadfb937482e732b34
Lev Leyman <lev.leiman@yandex.ru>
2018-11-03 13:47:27 +0400

    add mongodb

--- config/database.php
+++ config/database.php
@@ -78,6 +78,18 @@ return [
             'prefix' => '',
         ],
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGODB_HOST', 'mongo'),
+            'port'     => env('MONGODB_PORT', 27017),
+            'database' => env('MONGODB_DATABASE'),
+            'username' => env('MONGODB_USERNAME'),
+            'password' => env('MONGODB_PASSWORD'),
+            'options'  => [
+                'database' => 'admin',
+            ],
+        ],
+
     ],
 
     /*
